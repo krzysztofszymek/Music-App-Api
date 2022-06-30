b0526dfd18658802d35ac5501c471e6c4799e898
Krzysztof Szymek <63580276+krzysztofszymek@users.noreply.github.com>
2022-06-30 12:38:14 +0200

    User DTO

---
+++ DTOs/User/CreateUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_App_Api.DTOs.User
+{
+    public class CreateUserDTO
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+    }
+}
---
+++ DTOs/User/UpdatePasswordDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_App_Api.DTOs.User
+{
+    public class UpdatePasswordDTO
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
